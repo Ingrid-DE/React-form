4814fe71540a86fc47db241f2940cb5bf3eba12c
Ingrid Dahiana Enriquez Dos Santos <ingridenriquez74@gmail.com>
2024-08-19 17:22:54 -0400

    Original

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react"; 
+function App() {
+  
+  const [inputs, setInputs] = useState({});
+  
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert( `Tu pedido fue guardado como: ${JSON.stringify(inputs)}`);
+    console.log(inputs);
+  } 
+  
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-2">
+          <form onSubmit={handleSubmit}>
+      <div class="card-body bg-danger text-white">Floreria Rojas </div>
+      <label>Cliente:
+      <input
+        type="text"
+        name="nombre"
+        value={inputs.nombre || ""}
+        onChange={handleChange}
+        />
+      /cami
+
+
+
+
+
+      
+      /Juanjo
+
+
+
+
+
+
+      /Ara
+
+
+
+
+
+    /dani
+
+
+
+
+
+
+
+
+      </label>
+      <button className="btn-success" type="submit">Pedir</button>
+    </form>          
+        </div>
+      </div>
+    </div>
+        );
+      }
+      
+      export default App;
