5e3185059ae362dae02da48aa3c2da9b16d77b32
el_toro <jjchaparrox@gmail.com>
2024-08-22 19:57:40 -0400

    fix: cambio de juan en esta rama

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,15 @@ function App() {
         onChange={handleChange}
         />
       </label>
-      <button className="btn-success" type="submit">Comprar</button>
+      <label>Tipo de ramo:
+      <input
+        type="text"
+        name="tiporamo"
+        value={inputs.tiporamo || ""}
+        onChange={handleChange}
+        />
+      </label>
+      <button className="btn-success" type="submit">comprar</button>
     </form>          
         </div>
       </div>
