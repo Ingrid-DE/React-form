5849d4512751282e1c1bf19a046d16e5374c6671
Ingrid Dahiana Enriquez Dos Santos <ingridenriquez74@gmail.com>
2024-08-23 19:30:34 -0400

    add: mas prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-<label>Precios
-  <input 
+import { useState } from "react"; 
+function App() {
+
+  const [inputs, setInputs] = useState({});
+  
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert( `Tu compra fue guardado como: ${JSON.stringify(inputs)}`);
+    console.log(inputs);
+  } 
+  
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-2">
+          <form onSubmit={handleSubmit}>
+      <div class="card-body bg-danger text-white">Floreria Rojas </div>
+      <label>Comprador:
+      <input
   type="number" 
   name="precios"
   value={inputs.precios || ""}
     onchange={handlechange}
     />
-  </label>
+  </label>      <button className="btn-success" type="submit">Comprar</button>
+    </form>          
+        </div>
+      </div>
+    </div>
+        );
+      }
+      
+      export default App;
